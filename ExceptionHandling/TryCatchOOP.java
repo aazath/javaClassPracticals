c6e7499b909bca43633e0de9b99ae898d7e633bf
aazath <asaazath@gmail.com>
2022-10-25 11:40:41 +0530

    TryCatchOOP

---
+++ ExceptionHandling/TryCatchOOP.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Alpha{
+	public void division()
+	{
+		try
+		{
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Welcome to our Application");
+			System.out.println("==========================\n");
+			System.out.println("Enter the first number to divide :");
+			int first = scan.nextInt();
+			System.out.println("Enter the second number to divide :");
+			int second = scan.nextInt();
+			
+			int res = first/second;
+			
+			System.out.println("The result is "+res);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Enter a non-zero second number");
+		}
+		System.out.println("Thank you for using our application");
+	}
+}
+
+class Beta{
+	public void display()
+	{
+		Alpha a = new Alpha();
+		a.division();
+		
+	}
+}
+
+
+public class TryCatchOOP {
+
+	public static void main(String[] args) {
+		Beta b = new Beta();
+		b.display();
+
+	}
+
+}
