548459aa7159e8abb5c49e4763720ce3af19cb72
aazath <asaazath@gmail.com>
2022-10-25 10:39:13 +0530

    Arithmetic Exception Handling

--- ExceptionHandling/src/TryCatch.java
+++ ExceptionHandling/src/TryCatch.java
@@ -1,9 +1,27 @@
 package src;
 
+import java.util.Scanner;
+
 public class TryCatch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Welcome to AyaINN Application");
+		System.out.println("Enter the first number to devide: ");
+		int first = scan.nextInt();
+		System.out.println("Enter the second number to devide: ");
+		int second = scan.nextInt();
+		
+		try
+		{
+			System.out.println("The Result of the Division "+(first/second));
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Unable to Devide, Enter a non-zero second number.");
+		}
+		
 
 	}
 
