ac9e997edd7b48af0d0d2aff836c63d0d9c9d63b
aazath <asaazath@gmail.com>
2022-11-07 12:20:18 +0530

    detailed comments added

--- MT/DeadLockExample.java
+++ MT/DeadLockExample.java
@@ -1,5 +1,5 @@
 //This java application will use the concept of dead lock 
-//in multi threading
+//in multi-threading
 //synchronized - used to lock the resource only for one thread
 
 class Warrior implements Runnable
@@ -9,13 +9,17 @@ class Warrior implements Runnable
 	String res2 = new String("Axe");
 	String res3 = new String("Hoe");
 	
-	
-	
+	//thread scheduler will select a thread to run, that thread will 
+	//get the chance to execute the run()
+	//other thread will be moved to blocked state
 	public void run()
 	{
+		//get the name of the current thread
 		String curThread = Thread.currentThread().getName();
 		
 		try {
+			//if Tom thread get chance then run tomAcq()
+			//else run the jerryAcq()
 			if (curThread.equals("Tom")) {
 				tomAcq();
 			}
@@ -29,10 +33,22 @@ class Warrior implements Runnable
 	}
 
 
-	//warrior 1 gets the weapon
+	//Tom gets the weapon
 	public void tomAcq()
 	{
 		try {
+			//once the thread encounter the sleep state
+			//it will moved to sleep state from running state
+			//TS will give chance to the other thread
+			//in other thread the first line also sleep method
+			//so, the other thread also will be moved to the sleep state
+			//in mean time, this thread's sleep time will get over
+			//and let's assume this thread execute the synchronized(res1)
+			//on the other hand, the next thread's sleep time over
+			//and try to execute the synchronized(res1)
+			//this already locked by the first thread
+			//so, the second thread will move to the blocked state due to resource unavailability
+			//and will come out of of the blocked state after the resources are released by the first thread
 			Thread.sleep(4000);
 			
 			//gets the resource
@@ -43,7 +59,8 @@ class Warrior implements Runnable
 				
 				//wait for some more time to get more weapon
 				Thread.sleep(4000);
-				
+				//this thread will moved to the sleep state
+				//and moved to the runnable state after the sleep time is over
 				synchronized(res2)
 				{
 					System.out.println("Tom acquired "+res2);
@@ -68,13 +85,15 @@ class Warrior implements Runnable
 	}
 
 
-	//warrior 2 gets the weapon
+	//Jerry gets the weapon
 	public void jerryAcq()
 	{
 		try {
 			Thread.sleep(4000);
-			
 			//gets the resource
+			//in case, the res1 is already acquired by the other thread
+			//this thread will moved to the blocked state
+			//if res1 is available state
 			synchronized(res1)
 			{
 				System.out.println("Jerry acquired "+res1);
@@ -110,14 +129,18 @@ class Warrior implements Runnable
 public class DeadLockExample {
 
 	public static void main(String[] args) {
+		//create the warrior object
 		Warrior wr = new Warrior();
 		
+		//create 2 separate threads by passing the object
 		Thread tom = new Thread(wr);
 		Thread jerry = new Thread(wr);
 		
+		//set names to the threads
 		tom.setName("Tom");
 		jerry.setName("Jerry");
 		
+		//run the threads
 		tom.start();
 		jerry.start();
 
