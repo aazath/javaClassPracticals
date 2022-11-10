1e450f58cd15bd418b843573499df3f24f30ee70
aazath <asaazath@gmail.com>
2022-11-10 10:41:13 +0530

    Producer - consumer problem

---
+++ MT/ThreadComm.java
@@ -0,0 +1,90 @@
+//This java application demonstrates the communication b/w the threads
+
+//producer thread operation
+class Producer extends Thread
+{
+		// Producer producing the data in StringBuffer
+		StringBuffer sb;
+
+		// variable used by thread for Communication
+		boolean dataProvider = false;
+
+		public Producer() {
+			// StringBuffer object is created with a default capacity 16
+			sb = new StringBuffer();
+		}
+
+		@Override
+		public void run() {
+
+			for (int i = 1; i <= 10; i++) {
+				try {
+					sb.append(i + ": ");
+					Thread.sleep(100);
+					System.out.println("appending");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+			// update the consumer once data produced
+			dataProvider = true;
+
+		}
+	
+}
+
+
+//consumer thread operations
+class Consumer extends Thread
+{
+		// Creating producer object to get the produced data from StringBuffer
+		Producer producer;
+		
+		
+		//injecting the Producer Object into Consumer - DI
+		public Consumer(Producer producer) {
+			this.producer = producer;
+		}
+
+
+		@Override
+		public void run() {
+			
+			//check for the producer dataProvider for the dataProvider variable
+			//if true only consume the data otherwise enter into sleeping state
+			while(producer.dataProvider == false) {
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			//consume the data produced by the producer
+			System.out.println(producer.sb);
+
+		}
+}
+
+////Inefficient way of inter-thread communication 
+public class ThreadComm {
+
+	public static void main(String[] args) {
+
+			Producer obj1 = new Producer();
+			Consumer obj2 = new Consumer(obj1);
+			
+			
+			Thread t1 = new Thread(obj1);//producer thread
+			Thread t2 = new Thread(obj2);//consumer thread
+			
+			t2.start();//consumer should wait
+			t1.start();//producer should start
+				
+		}
+
+}
+
+
