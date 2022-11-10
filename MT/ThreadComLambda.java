c62e550e4d7a39c4b06a4c399348d9a4921c8ee1
aazath <asaazath@gmail.com>
2022-11-10 13:33:02 +0530

    Lambda Expression coding of thread communication

--- MT/ThreadComLambda.java
+++ MT/ThreadComLambda.java
@@ -5,17 +5,31 @@ public class ThreadComLambda {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Anonymous inner class
-		new Thread(new Runnable() {
-			public void run()
-			{
-				for (int i = 1; i <= 5; i++) {
-					try {
-						System.out.println("Child thread");
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+//		//Anonymous inner class
+//		new Thread(new Runnable() {
+//			public void run()
+//			{
+//				for (int i = 1; i <= 5; i++) {
+//					try {
+//						System.out.println("Child thread");
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		}).start();
+		
+		//Lambda expression
+		//new Thread(()->{//program logic}).start();
+		new Thread(() -> {
+			//body of the method
+			for (int i = 1; i <= 5; i++) {
+				try {
+					System.out.println("Child thread");
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
 		}).start();
