e3817e0bf2e2531d50c1abf09a3f905688eaaa45
aazath <asaazath@gmail.com>
2022-11-10 10:41:40 +0530

    Daemon Thread

---
+++ MT/DaemonThreadEx.java
@@ -0,0 +1,12 @@
+
+public class DaemonThreadEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Thread t = Thread.currentThread();
+		System.out.println(t.isDaemon());
+
+	}
+
+}
