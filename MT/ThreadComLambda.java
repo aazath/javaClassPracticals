368ab3e1720b85c453204709f71abd2cf1f5e934
aazath <asaazath@gmail.com>
2022-11-10 12:23:17 +0530

    Using anonymous inner class

---
+++ MT/ThreadComLambda.java
@@ -0,0 +1,35 @@
+//This java application will use lambda expression to communicate with other threads
+//minimum class files need minimum resources to run the application
+public class ThreadComLambda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Anonymous inner class
+		Thread thread = new Thread(new Runnable() {
+			public void run()
+			{
+				for (int i = 1; i <= 5; i++) {
+					try {
+						System.out.println("Child thread");
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread.start();
+		
+		for (int i = 1; i <= 5; i++) {
+			try {
+				System.out.println("Main thread");
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
