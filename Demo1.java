3da003f0ea4b17d3c21b0e3d4cb0339ba07bef30
aazath <asaazath@gmail.com>
2022-11-23 17:31:54 +0530

    Demo added

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Demo1{
+
+public static void main(String[]args){
+        List<Integer> nums = Arrays.asList(6,5,2,8,1,7);
+        System.out.println(nums); 
+}
+}
